485d470ac0ecdd5de497effb93030a80ab5a0931
Phelipe Luiz <35576221+phelipe-plda@users.noreply.github.com>
2022-04-20 22:26:19 -0300

    first commit

---
+++ src/main/java/Principal.java
@@ -0,0 +1,27 @@
+import com.manoelcampos.exportador.ExportadorListaProduto;
+import com.manoelcampos.exportador.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Executa a aplicação Exportador Simple Factory.
+ * @author Manoel Campos da Silva Filho
+ */
+public class Principal {
+    public static void main(String[] args) {
+        final List<Produto> produtos = new ArrayList<>();
+        produtos.add(new Produto("TV", "LG", "1234-a", 1200));
+        produtos.add(new Produto("Geladeira", "Electrolux", "Delux", 400));
+        produtos.add(new Produto("Notebook", "Asus", "Predator", 850));
+        produtos.add(new Produto("Celular ", "Apple", "Iphone 11", 3850));
+
+        ExportadorListaProduto exportadorPadrao = ExportadorListaProduto.newInstance();
+        System.out.println("Lista de Produtos em HTML\n");
+        System.out.println(exportadorPadrao.exportar(produtos));
+
+        ExportadorListaProduto exportadorMarkdown = ExportadorListaProduto.newInstance("md");
+        System.out.println("Lista de Produtos em Markdown\n");
+        System.out.println(exportadorMarkdown.exportar(produtos));
+    }
+}
